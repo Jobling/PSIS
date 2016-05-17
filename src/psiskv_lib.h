73fc72b1b2cf360191495dd6ec144ea6a8dd223b
Jobling <tiago.jobling@gmail.com>
2016-05-17 08:38:58 +0100

    Header files are back!

---
+++ src/psiskv_lib.h
@@ -0,0 +1,22 @@
+#ifndef __PSISKV_LIB__
+#define __PSISKV_LIB__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "message.h"
+
+int kv_connect(char * kv_server_ip, int kv_server_port);
+void kv_close(int kv_descriptor);
+int kv_write(int kv_descriptor, uint32_t key, char * value, int value_length, int kv_overwrite);
+int kv_read(int kv_descriptor, uint32_t key, char * value, int value_length);
+int kv_delete(int kv_descriptor, uint32_t key);
+
+#endif
+
