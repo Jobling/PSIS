330750f23cf3cc49d965051fb8614887a904af82
Jobling <tiago.jobling@gmail.com>
2016-05-27 01:47:10 +0100

    Front server ready to work. Not tested.

--- src/psiskv_front_server.c
+++ src/psiskv_front_server.c
@@ -11,6 +11,7 @@
 int data_listener = -1;
 int front_sock;
 int count = 0;
+int listener;
 struct sockaddr_un peer;
 
 /* Handle SIGINT signal to perform
@@ -28,9 +29,8 @@ void sig_handler(int sig_number){
 
 /* Handle incoming receives from data server */
 void * heartbeat_recv(void * arg){
-	char buffer[BUFFSIZE];
 	while(1){
-		recv(front_sock, buffer, BUFFSIZE, 0);
+		recv(front_sock, &data_listener, sizeof(int), 0);
 		printf("DATA SERVER IS ALIVE. For now...\n");
 		count = 0;
 	}
@@ -43,14 +43,46 @@ void * heartbeat_send(void * arg){
 		count++;
 		if(count == TIMEOUT){
 			printf("DATA SERVER IS DEAD!\nRIP X.X\n");
+			// data_listener = -1;
 			sig_handler(SIGINT);
 		}
 		sleep(1);
 	}
 }
 
+/* Initialize listening socket listener (UDP)
+ *
+ * This function returns listening socket on success
+ * This function returns -1 if an error occurs */
+int server_init(){
+	int listener;
+	struct sockaddr_in local_addr;
+
+	/* Create socket */
+	if((listener = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		perror("Socket");
+		return -1;
+	}
+
+	/* Create address */
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_port = htons(LISTENER_PORT);
+	local_addr.sin_addr.s_addr = INADDR_ANY;
+
+	/* Bind socket to address */
+	if(bind(listener, (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1){
+		perror("Bind");
+		close(listener);
+		return -1;
+	}
+
+	printf("Front server ready to receive requests\n");
+	return listener;
+}
 
 int main(int argc, char ** argv){
+	int nbytes, ignore;
+	struct sockaddr_in client;
 	pthread_t heartbeat[2];
 	// pid_t call_data
 
@@ -73,10 +105,33 @@ int main(int argc, char ** argv){
 	}
 
 	if(pthread_create(&heartbeat[0], NULL, heartbeat_recv, NULL) != 0){
-		perror("Creating thread");
+		perror("Creating heartbeat thread");
 		sig_handler(SIGINT);
 	}
 
-	heartbeat_send(NULL);
+	if(pthread_create(&heartbeat[1], NULL, heartbeat_send, NULL) != 0){
+		perror("Creating heartbeat thread");
+		sig_handler(SIGINT);
+	}
+
+	listener = server_init();
+	if(listener == -1){
+		exit(-1);
+	}
+
+	while(1){
+		nbytes = recvfrom(listener, &ignore, sizeof(int), 0, (struct sockaddr *) &client, sizeof(client));
+		if(nbytes > 0){
+			nbytes = sendto(listener, &data_listener, sizeof(int), 0, (struct sockaddr *) &client, sizeof(client));
+			if(nbytes <= 0){
+				perror("Sending replies");
+				exit(-1);
+			}
+		}else{
+			perror("Receiving requests");
+			exit(-1);
+		}
+	}
+
     exit(0);
 }
